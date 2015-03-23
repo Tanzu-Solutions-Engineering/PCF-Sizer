8d3bf561442bcec6984e4282050bf744ab213a8d
krujos <jkruck@pivotal.io>
2015-03-23 10:26:07 -0500

    Add in sizing

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,6 +3,10 @@ var shekelApp = angular.module('ShekelApp', []);
 
 shekelApp.controller('ShekelController', function($scope, $http) {
 
+    $scope.aiPacks = [50, 100, 150, 200, 250, 300];
 
+    $scope.avgRam = [.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 10, 20, "too much"];
+    
+    $scope.deaSize = ["Small", "Medium", "Large", "Bad idea"]
 });
 
