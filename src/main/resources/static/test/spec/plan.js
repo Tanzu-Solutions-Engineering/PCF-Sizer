992321c5e45503e54cc811c1e78baf6a7587a9e6
krujos <jkruck@pivotal.io>
2015-04-10 13:31:22 -0500

    implement some default plan stuff

--- src/main/resources/static/test/spec/plan.js
+++ src/main/resources/static/test/spec/plan.js
@@ -1,15 +1,38 @@
+'use strict';
+
 (function() {
 
-    var createController;
+    describe('ShekelPlanController', function() {
 
-    beforeEach(module('ShekelApp'));
+        var $rootScope, createController;
 
-    beforeEach(inject(function($injector) {
-        $rootScope = $injector.get('$rootScope');
-        var $controller = $injector.get('$controller');
+        beforeEach(module('ShekelApp'));
 
-        createController = function () {
-            return $controller('ShekelPlanController', {'$scope': $rootScope});
-        };
-    }));
-});
+        beforeEach(inject(function($injector) {
+            $rootScope = $injector.get('$rootScope');
+            var $controller = $injector.get('$controller');
+
+            createController = function () {
+                return $controller('ShekelPlanController', {'$scope': $rootScope});
+            };
+        }));
+
+
+        describe('defaults', function() {
+            beforeEach(function() { createController() });
+
+            it('should start with a default plan', function () {
+                expect($rootScope.plan).toBeDefined();
+            });
+
+            it('should not have any plans defined', function() {
+                expect($rootScope.plans.length).toBe(0);
+            });
+
+            it('should have the plan form hidden', function() {
+                expect($rootScope.showPlanForm).toBeFalsy();
+            })
+        });
+
+    });
+})();
