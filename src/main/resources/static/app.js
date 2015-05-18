1fff687af77e8fa67a48700319538536c3ae6b48
krujos <jkruck@pivotal.io>
2015-05-18 11:38:21 -0500

    Add node implementation so we can get rid of java

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('.'));
+
+app.get('/buildnumber', function(req, res) {
+    var vcapApplication = process.env.VCAP_APPLICATION
+        || '{ "application_name": "local-dev", "application_uris": [ "localhost:3000" ] }';
+
+    res.send(vcapApplication);
+});
+app.listen(process.env.PORT || 3000);
