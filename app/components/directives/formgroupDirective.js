e3d284eccc35254216d00e31928934820608eb45
krujos <jkruck@pivotal.io>
2015-05-20 00:23:59 -0400

    Layout

--- app/components/directives/formgroupDirective.js
+++ app/components/directives/formgroupDirective.js
@@ -3,8 +3,8 @@ shekelApp.directive('shklFormGroup', function () {
         scope: {
             label: '@label',
             value: '@value',
-            type: '@type'
-            //id: "@generated_value" TODO Need to come up with a way to generate ID's so we don't break screen reades
+            type: '@type',
+            //id: "@generated_value" TODO Need to come up with a way to generate ID's so we don't break screen readers
         },
         restrict: 'E',
         templateUrl: '/app/components/directives/formgroup.html'
@@ -16,8 +16,8 @@ shekelApp.directive('shklNumInputFormGroup', function () {
         scope: {
             label: '@label',
             value: '@value',
-            ngModel: '='
-            //id: "@generated_value" TODO Need to come up with a way to generate ID's so we don't break screen reades
+            ngModel: '=',
+            //id: "@generated_value" TODO Need to come up with a way to generate ID's so we don't break screen readers
         },
         restrict: 'E',
         templateUrl: '/app/components/directives/num-input-form-group.html'
