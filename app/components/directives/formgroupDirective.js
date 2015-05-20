ff489272a8ddc2e1be13d6aa32d94f1ae1c08fd7
krujos <jkruck@pivotal.io>
2015-05-19 21:57:13 -0400

    updates for ai form

--- app/components/directives/formgroupDirective.js
+++ app/components/directives/formgroupDirective.js
@@ -3,9 +3,23 @@ shekelApp.directive('shklFormGroup', function () {
         scope: {
             label: '@label',
             value: '@value',
+            type: '@type'
             //id: "@generated_value" TODO Need to come up with a way to generate ID's so we don't break screen reades
         },
         restrict: 'E',
         templateUrl: '/app/components/directives/formgroup.html'
+    };
+});
+
+shekelApp.directive('shklNumInputFormGroup', function () {
+    return {
+        scope: {
+            label: '@label',
+            value: '@value',
+            ngModel: '='
+            //id: "@generated_value" TODO Need to come up with a way to generate ID's so we don't break screen reades
+        },
+        restrict: 'E',
+        templateUrl: '/app/components/directives/num-input-form-group.html'
     };
 });
