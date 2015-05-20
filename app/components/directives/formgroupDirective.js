f0931d13e5d3b5862f6243247a682435005f13da
krujos <jkruck@pivotal.io>
2015-05-20 00:52:01 -0400

    Checkin sizing work so far

---
+++ app/components/directives/formgroupDirective.js
@@ -0,0 +1,25 @@
+shekelApp.directive('shklFormGroup', function () {
+    return {
+        scope: {
+            label: '@label',
+            value: '@value',
+            type: '@type',
+            //id: "@generated_value" TODO Need to come up with a way to generate ID's so we don't break screen readers
+        },
+        restrict: 'E',
+        templateUrl: '/app/components/directives/formgroup.html'
+    };
+});
+
+shekelApp.directive('shklNumInputFormGroup', function () {
+    return {
+        scope: {
+            label: '@label',
+            value: '@value',
+            ngModel: '=',
+            //id: "@generated_value" TODO Need to come up with a way to generate ID's so we don't break screen readers
+        },
+        restrict: 'E',
+        templateUrl: '/app/components/directives/num-input-form-group.html'
+    };
+});
