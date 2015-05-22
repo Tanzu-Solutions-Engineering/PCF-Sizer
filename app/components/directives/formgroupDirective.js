2a7bc52a715606c885f34e4b93df8b089c79d1f9
krujos <jkruck@pivotal.io>
2015-05-22 10:37:49 -0500

    Costing form layout is more better

--- app/components/directives/formgroupDirective.js
+++ app/components/directives/formgroupDirective.js
@@ -28,9 +28,8 @@ shekelApp.directive('shklMonthlyCostingInput', function () {
     return {
         scope: {
             label: '@label',
-            labelModel: '=',
+            inputModel: '=',
             selectModel: '='
-            //id: "@generated_value" TODO Need to come up with a way to generate ID's so we don't break screen readers
         },
         restrict: 'E',
         templateUrl: '/app/components/directives/monthly-costing-input.html'
