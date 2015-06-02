14b1e946fe1e1ec972fe9d880d23df726450af89
krujos <jkruck@pivotal.io>
2015-06-01 20:22:41 -0500

    Fix build break

--- js/main.js
+++ js/main.js
@@ -1,46 +1,43 @@
 "use strict"
-$(window).load(function () {
-	$('#myModal').modal('show');
-});
 
 var shekelApp = angular.module('ShekelApp', []);
 
-var vmService = shekelApp.factory('vmLayout', function($rootScope) {
-	var vmLayout = new Array(); 
-	return vmLayout;
+var vmService = shekelApp.factory('vmLayout', function ($rootScope) {
+    var vmLayout = new Array();
+    return vmLayout;
 });
 
-var aiService = shekelApp.factory('aiService', function($rootScope) {
-	var aiPacks = {};
-	
-	function setAiPacks(pack) { 
-		aiPacks = pack;
-	}
-	
-	function getAiPacks() { 
-		return aiPacks;
-	}
-	
-	return { 
-		aiPacks: getAiPacks, 
-		setAiPack: setAiPacks
-	}
-	
+var aiService = shekelApp.factory('aiService', function ($rootScope) {
+    var aiPacks = {};
+
+    function setAiPacks(pack) {
+        aiPacks = pack;
+    }
+
+    function getAiPacks() {
+        return aiPacks;
+    }
+
+    return {
+        aiPacks: getAiPacks,
+        setAiPack: setAiPacks
+    }
+
 });
 
-shekelApp.controller('ShekelFoundationController', function($scope) {
-	$scope.singleComplianceZone = "yes";
-	$scope.seperateForProd = "yes";
-	$scope.complianceZones = 1;
-	$scope.physicalDC = 1;
-		
-	$scope.foundations = function() { 
-		var multiplier = $scope.seperateForProd == "yes" ? 2 : 1;
-		//Gross side effect... 
-		if ( $scope.singleComplianceZone == "yes") { 
-			$scope.complianceZones = 1;
-		}
-		return $scope.physicalDC * $scope.complianceZones * multiplier; 
-	}
+shekelApp.controller('ShekelFoundationController', function ($scope) {
+    $scope.singleComplianceZone = "yes";
+    $scope.seperateForProd = "yes";
+    $scope.complianceZones = 1;
+    $scope.physicalDC = 1;
+
+    $scope.foundations = function () {
+        var multiplier = $scope.seperateForProd == "yes" ? 2 : 1;
+        //Gross side effect...
+        if ($scope.singleComplianceZone == "yes") {
+            $scope.complianceZones = 1;
+        }
+        return $scope.physicalDC * $scope.complianceZones * multiplier;
+    }
 });
 
