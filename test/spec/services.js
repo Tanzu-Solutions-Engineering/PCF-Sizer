f73dc3624a4c666707fa3920c258a6496b8a1907
krujos <jkruck@pivotal.io>
2015-12-02 07:11:57 -0600

    Failing test for client side consumption of mysql

---
+++ test/spec/services.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function() {
+
+    describe('ShekelServicesController', function() {
+        it('should fail', function() {
+            fail('nyi');
+        });
+    })
+})();
