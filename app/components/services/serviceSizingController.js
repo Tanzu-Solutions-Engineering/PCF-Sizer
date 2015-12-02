cd66e464166b6c8dc735ec654ffc537537be123b
krujos <jkruck@pivotal.io>
2015-12-02 08:30:07 -0600

    add service versions

--- app/components/services/serviceSizingController.js
+++ app/components/services/serviceSizingController.js
@@ -14,4 +14,14 @@ shekelApp.controller('ShekelServiceSizingController', function($scope, $http) {
 
     $scope.services();
 
+    /**
+     * Returns a promise which will contain an array of versions.
+     * @param serviceName
+     * @returns {*}
+     */
+    $scope.getServiceVersions = function(serviceName) {
+        var url = '/services/' + serviceName + '/versions';
+        return $http.get(url).then(function(data) {return data.data;});
+    };
+
 });
--- test/spec/services.js
+++ test/spec/services.js
@@ -4,11 +4,14 @@
     describe('ShekelServicesController', function() {
         var $rootScope, createController, $httpBackend;
         var services = ['mysql', 'gemfire', 'rabbit', 'redis'];
+        var mysqlVersions = ['1.7', '1.6.5', '1.6.4', '1.3'];
+
         beforeEach(module('ShekelApp'));
 
         beforeEach(inject(function($injector) {
             $httpBackend = $injector.get('$httpBackend');
             $httpBackend.when('GET', '/services').respond(services);
+            $httpBackend.when('GET', '/services/mysql/versions').respond(mysqlVersions);
             $rootScope = $injector.get('$rootScope');
             var $controller = $injector.get('$controller');
             createController = function() {
@@ -16,26 +19,50 @@
             }
         }));
 
-        beforeEach(function() {
-            $httpBackend.expectGET('/services');
-            createController();
-            $httpBackend.flush();
-        });
-
         afterEach(function() {
             $httpBackend.verifyNoOutstandingExpectation();
             $httpBackend.verifyNoOutstandingRequest();
         });
 
+        describe('listing services', function() {
 
-        it('should return a mysql and gemfire service', function() {
-            var returnedServices = $rootScope.services();
-            expect(returnedServices).toContain('mysql');
-            expect(returnedServices).toContain('gemfire');
+            beforeEach(function() {
+                $httpBackend.expectGET('/services');
+                createController();
+                $httpBackend.flush();
+            });
+
+            it('should return a mysql and gemfire service', function() {
+                var returnedServices = $rootScope.services();
+                expect(returnedServices).toContain('mysql');
+                expect(returnedServices).toContain('gemfire');
+            });
+
+            it ('should return as many services as the api returns', function() {
+                expect($rootScope.services().length).toBe(services.length)
+            });
         });
 
-        it ('should return as many services as the api returns', function() {
-            expect($rootScope.services().length).toBe(services.length)
-        })
+        describe('listing versions', function() {
+            var versions = null;
+
+            beforeEach(function() {
+                $httpBackend.expectGET('/services/mysql/versions');
+                createController();
+                $rootScope.getServiceVersions('mysql').then(function(v) {
+                    versions = v;
+                });
+                $httpBackend.flush();
+
+            });
+
+            it('Should return all the mysql versions', function() {
+                expect(versions.length).toBe(mysqlVersions.length);
+            });
+
+            it('should contain version 1.7', function() {
+                expect(versions).toContain('1.7');
+            })
+        });
     })
 })();
