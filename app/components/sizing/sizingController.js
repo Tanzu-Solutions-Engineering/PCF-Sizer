754ccf917bd6940799b1a2c0cb31f45b13ca22f6
krujos <jkruck@pivotal.io>
2015-12-06 15:43:16 -0600

    remove unused recovery capacity stuff

--- app/components/sizing/sizingController.js
+++ app/components/sizing/sizingController.js
@@ -98,9 +98,7 @@ shekelApp.controller('ShekelSizingController', function($scope, $http, tileServi
     	pcfCompilationJobs: $scope.pcfCompilationJobsOptions[4],
     	iaasCPUtoCoreRatio: $scope.iaasCPUtoCoreRatioOptions[1]
     };
-
-    $scope.aZRecoveryCapacity = [25, 50, 100];
-
+    
     $scope.chooser = { aiHelpMeChoose: false };
 
     $scope.aiChooser = { 
