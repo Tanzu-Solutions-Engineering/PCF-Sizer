17dfe02361b5f8ca41322a947e673511c7921869
Brian Byers <bbyers@pivotal.io>
2016-07-13 22:28:02 -0500

    testing pipeline

---
+++ ci/scripts/unit.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+echo "node version $(node -v) running"
+echo "npm version $(npm -v) running"
+
+npm config set unsafe-perm true
+echo "{\"allow_root\": true}" > ~/.bowerrc
+
+cd git-repo
+npm install
+npm install -g bower
+npm install -g karma
+npm install -g karma-jasmine --save-dev
+npm install -g karma-phantomjs-launcher
+npm install gulp
+bower install
+
+npm test
